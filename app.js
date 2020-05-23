569f12ba7b5d791ede76dfba84004e4658df7669
ayushsri15 <62467766+ayushsri15@users.noreply.github.com>
2020-05-23 06:22:16 +0530

    DB ,ProcFile,Updates in app.js home.ejs

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
+
+
+
+
 
 
 
@@ -11,64 +16,100 @@ const homeStartingContent = "Hey Guys! this is an Under Construction Website For
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts =[];
-const postContent="";
+
+const postContent = "";
 
 const app = express();
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-
-  res.render("home",{homeContent : homeStartingContent,posts : posts });
+// connecting mongoose-mongodb with the app
+mongoose.connect("mongodb+srv://ayush-blogs:sunshine123@ayush-blogscluster-imtd7.mongodb.net/blogsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
-app.get("/about",function(req,res){
-  res.render("about",{about:aboutContent});
+
+
+// creating schema
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String
 });
-app.get("/contact",function(req,res){
-  res.render("contact",{contact:contactContent});
-});
-app.get("/compose",function(req,res){
-  res.render("compose");
 
-});
-app.post("/compose",function(req,res){
+const BlogPost = mongoose.model("Blog", blogSchema);
 
-  let post = {
-    title : req.body.titleContent,
-    content  : req.body.bodyContent
-  };
 
-  posts.push(post);
-  res.redirect("/");
-});
-app.get("/posts/:postName",function(req,res){
 
-  const requestTitle= req.params.postName;
-  posts.forEach(function(post){
-//     if (posts.content.length > 100) {
-//        postContent =posts.content.substring(0, 100) + "...";
-//     }
-//     else {
-//       postContent = posts.content;
-//
-// }
-
-    if(_.lowerCase(post.title)===_.lowerCase(requestTitle))
-    {
-      res.render("post",{
-        title:post.title,
-        content:post.content
+app.get("/", function(req, res) {
+  BlogPost.find({}, function(err, posts) {
+    if (!err) {
+      res.render("home", {
+        homeContent: homeStartingContent,
+        posts: posts
       });
     }
   });
-});
 
 
-app.listen(process.env.PORT||3000, function(){
-  console.log("Server 3000 is running");
 });
+app.get("/about", function(req, res) {
+  res.render("about", {
+    about: aboutContent
+  });
+});
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contact: contactContent
+  });
+});
+app.get("/compose", function(req, res) {
+  res.render("compose");
+
+});
+app.post("/compose", function(req, res) {
+
+  let post = {
+    title: req.body.titleContent,
+    content: req.body.bodyContent
+  };
+
+
+
+  const blogPost = new BlogPost({
+    title: req.body.titleContent,
+    content: req.body.bodyContent
+  });
+  blogPost.save(function(err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+app.get("/posts/:postId", function(req, res) {
+
+      const requestTitle = _.capitalize(req.params.postName);
+      const requestedPostId = req.params.postId;
+      BlogPost.findOne({
+          _id: requestedPostId
+        }, function(err, post) {
+          if (!err) {
+              res.render("post", {
+                title: post.title,
+                content: post.content
+              });
+            }
+          }
+        );
+
+    });
+    app.listen(process.env.PORT || 5000, function() {
+      console.log("Server 3000 is running");
+    });
